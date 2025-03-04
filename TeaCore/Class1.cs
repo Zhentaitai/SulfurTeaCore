0395241ae785f0bb1df1470622c881ce9a7f246e
子峰 徐 <2045228490@qq.com>
2025-03-04 22:56:52 +0800

    HelloWorld Init

---
+++ TeaCore/Class1.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace HelloMod
+{
+    [BepInPlugin("me.zhentai.plugin.HelloMod", "HelloWorld", "1.0.0")]
+    public class HelloWorld : BaseUnityPlugin
+    {
+        void Awake() 
+        {
+            UnityEngine.Debug.Log("Unity: Hello World!");
+            Logger.LogInfo("BepInEx: Hello World!");
+        }
+    }
+}
